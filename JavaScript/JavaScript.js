17e645acf87c6a4dcc7c75a938c72b180d71d8d5
sam990 <sam861g@gmail.com>
2019-05-04 00:43:29 -0700

    added working cancel btn to donor page add activity form

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,6 +1,4 @@
-
-$(document).ready(function () {
-
+$(document).ready(function() {
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBXpm4owlDd5Lh937ObtnvNHZ_2vDAk5VI",
@@ -8,7 +6,7 @@ $(document).ready(function () {
     databaseURL: "https://projectfamly.firebaseio.com",
     projectId: "projectfamly",
     storageBucket: "projectfamly.appspot.com",
-    messagingSenderId: "49552131620"
+    messagingSenderId: "49552131620",
   };
   firebase.initializeApp(config);
 
@@ -16,37 +14,31 @@ $(document).ready(function () {
   const auth = firebase.auth();
 
   // sign in
-  $('#loginBtn').click(function (e) {
+  $("#loginBtn").click(function(e) {
     e.preventDefault();
-    const email = $('#uname').val();
-    const pass = $('#upsw').val();
+    const email = $("#uname").val();
+    const pass = $("#upsw").val();
     const promise = auth.signInWithEmailAndPassword(email, pass);
 
     promise.catch(e => console.log(e.message));
-
-
   });
 
   // signup event
-  $('.signupbtn').click(function (e) {
+  $(".signupbtn").click(function(e) {
     e.preventDefault();
-    const email = $('#email').val();
-    const pass = $('#psw').val();
+    const email = $("#email").val();
+    const pass = $("#psw").val();
     const promise = auth.createUserWithEmailAndPassword(email, pass);
-    
+
     promise.catch(e => console.log(e.message));
-
-
   });
 
   auth.onAuthStateChanged(User => {
-
-    if (User){
-      // window.location = 'mentorpg.html';
-      console.log(User)
-    } 
-    else {
-      console.log('not logged in')
+    if (User) {
+      window.location = "mentorpg.html";
+      console.log(User);
+    } else {
+      console.log("not logged in");
     }
   });
 });
